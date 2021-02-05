Fixing splitting and masking methods

ops/train.py
@@ -13,7 +13,7 @@ from utils.scores import Scores
 
 class Trainer(object):
     # TODO: Consider designing Tuning and Benchmarking as subclasses of Trainer
-    def __init__(self, config: Dict, model: torch.nn.Module, dataset: Union[DataLoader], device):
+    def __init__(self, config: Dict, model: torch.nn.Module, dataset: Union[DataLoader], split_masks, device):
         self.run_config = config["run_config"]
         self.model = model
         self.params = torch.nn.ParameterList(self.model.parameters())
@@ -21,6 +21,7 @@ class Trainer(object):
         self.optimizer = OptimizerObj(config, self.params).optim_obj
         self.scheduler = LRScheduler(config, self.optimizer).schedule_obj
         self.dataset = dataset
+        self.split_masks = split_masks
         self.device = device
 
     def train(self, data, param_idx):