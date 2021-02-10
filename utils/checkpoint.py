f38afbb77f592f99d70b0411fb0fbf89803175a9
Flawnson Tong <flawnsontong1@gmail.com>
2021-02-10 00:22:10 -0500

    Added model checkpointing

---
+++ utils/checkpoint.py
@@ -0,0 +1,21 @@
+import torch
+
+from typing import *
+from logzero import logging
+
+
+def checkpoint(config: Dict, epoch: int, model: torch.nn.Module, loss: float, optimizer: torch.optim):
+    # Checkpoint intervals must be a divisor of the total number of epochs
+    if config["checkpoint_intervals"] is None:
+        pass
+    elif epoch % config["checkpoint_intervals"] == 0:
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': loss,
+        }, config["checkpoint_dir"])
+        logging.info(f"Successfully saved model checkpoint at epoch{epoch}. File saved at {config['checkpoint_dir']}")
+    else:
+        pass
+
