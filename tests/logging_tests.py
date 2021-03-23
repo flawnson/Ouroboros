1363d50ed717729f2cbfb1cee3122724b3ef979c
flawnson <flawnsontong1@gmail.com>
2021-03-23 11:23:48 -0400

    Implemented tb_logging test

---
+++ tests/logging_tests.py
@@ -0,0 +1,38 @@
+import os
+import json
+import torch
+import pytest
+import logzero
+import logging
+import argparse
+import numpy as np
+
+from typing import *
+from logzero import logger
+from utils.utilities import timed
+from utils.logging import TBLogger
+
+### Configuring ###
+
+
+@pytest.fixture
+def config():
+    file = r"C:\Users\flawn\Documents\Projects\Project_Introversion\Ouroboros\configs\local_linear_aux_demo.json"
+    config: Dict = json.load(open(file))
+    device = torch.device("cuda" if config["device"] == "cuda" and torch.cuda.is_available() else "cpu")
+    logzero.loglevel(eval(config["logging"]))
+    logger.info(f"Successfully retrieved config json. Running {config['run_name']} on {device}.")
+
+    return config
+
+
+def test_tb_logging(config):
+    tb_logger = TBLogger(os.path.join(config["log_dir"], config["run_name"]))
+    for idx in range(0, 100):
+        tb_logger.scalar_summary('Some metric', np.random.randint(0, 1000), idx)
+        tb_logger.scalar_summary('Some other metric', np.random.randint(0, 1000), idx)
+
+    assert os.path.exists(config["log_dir"])
+    assert len(os.listdir(config["log_dir"])) > 0
+
+
