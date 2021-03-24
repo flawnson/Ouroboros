2be39671f01bae1f54c4e167cc63a5bfe8e7cf62
flawnson <flawnsontong1@gmail.com>
2021-03-24 09:36:51 -0400

    TB kinda works; logs writing but not iteratively

--- utils/logging.py
+++ utils/logging.py
@@ -6,17 +6,24 @@ import os
 import pathlib
 import tensorflow as tf
 
+from logzero import logger
+
 
 class TBLogger(object):
 
     def __init__(self, config):
         """Create a summary writer logging to log_dir."""
         self.config = config
-        self.log_dir = os.path.join(config["log_dir"], config["run_name"])
+        self.log_dir = os.path.join(config["log_dir"], "TB_" + config["run_name"])
         pathlib.Path(self.log_dir).mkdir(parents=True, exist_ok=True)
         if config["clean_log_dir"] and len(os.listdir(self.log_dir)) > 0:  # Clears dir of old event files if True
             for f in os.listdir(self.log_dir):
-                os.remove(os.path.join(self.log_dir, f))
+                try:
+                    os.remove(os.path.join(self.log_dir, f))
+                    logger.info("Successfully cleaned TB log directory")
+                except Exception as e:
+                    logger.info(e)
+                    logger.info("Continuing run without deleting some files from log directory")
         self.writer = tf.summary.create_file_writer(self.log_dir)
 
     def scalar_summary(self, tag, value, step):
