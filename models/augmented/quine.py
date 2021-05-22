3676d20460b56f95346f22e7dba01eb1a1be94e8
Kevin Shen <kshen3778@gmail.com>
2021-05-22 11:30:33 -0400

    code optimizations for regenerate

--- models/augmented/quine.py
+++ models/augmented/quine.py
@@ -216,20 +216,21 @@ class Auxiliary(Vanilla, torch.nn.Module):
 
     @timed
     @torch.no_grad()
-    def regenerate(self):
+    def regenerate(self, coordinates, param_idx_map):
         """
         The regenerate, implemented by following the original Quine paper.
         Model parameters are kept in self.param_list and used for training and inference
         Due to the iteration, the model uses the regenerated version of itself to regenerate the next parameter.
         """
         index_list = list(range(self.num_params))
-        coordinates = self.indexer()
+        #coordinates = self.indexer()
         logger.info(f"Regenerating {len(coordinates)} parameters")
         for param_idx, coo in zip(index_list, coordinates):
             logger.info(f"Regenerating parameter {param_idx}")
             with torch.no_grad():
-                idx_vector = torch.squeeze(self.to_onehot(param_idx))  # Pulling out the nested tensor
-                predicted_param, predicted_aux = self.forward(idx_vector, None)
+                #idx_vector = torch.squeeze(self.to_onehot(param_idx))  # Pulling out the nested tensor
+                predicted_param = param_idx_map[param_idx] # extract the parameter value already calculated in training
+                #predicted_param, predicted_aux = self.forward(idx_vector, None)
                 new_params = deepcopy(self.param_list)
                 try:
                     # To catch ases where the parameter tensor is of size 0
@@ -246,8 +247,3 @@ class Auxiliary(Vanilla, torch.nn.Module):
         This method is meant to regenerate entire layers or models at a time rather than individual weights.
         """
         pass
-
-
-
-
-
