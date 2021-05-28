7fa685c601034313288edc45b3414824ab3927c3
flawnson <flawnsontong1@gmail.com>
2021-05-28 09:15:43 -0400

    Continued work on Transformer

--- models/standard/transformer_model.py
+++ models/standard/transformer_model.py
@@ -1,14 +1,16 @@
 import math
 import torch
 import numpy as np
+import torch.nn.functional as F
 
 from typing import *
 from logzero import logger
+from torch.nn import TransformerEncoder, TransformerEncoderLayer, TransformerDecoder, TransformerDecoderLayer
 
 
-class TransformerModel(torch.nn.Module):
+class ManualTransformerModel(torch.nn.Module):
     def __init__(self, config, device):
-        super(TransformerModel, self).__init__()
+        super(ManualTransformerModel, self).__init__()
         self.config = config
         self.device = device
         self.positional_encoding = PositionalEncoding(d_model, config["dropout"], config["max_len"])
@@ -18,10 +20,11 @@ class TransformerModel(torch.nn.Module):
         k = torch.nn.Linear()(k)
         q = torch.nn.Linear()(q)
 
+        # Scaled Dot Product Attn
         qk = torch.matmul(q, k)
         scaled_qk = qk / torch.sqrt(torch.tensor(k.size()))
         masked_qk = scaled_qk
-        agg_qk = torch.softmax(masked_qk)
+        agg_qk = torch.softmax(masked_qk, dim=-1)  # Or dim=2, softmax needs to be applied across the time dim
         out = torch.matmul(agg_qk, v)
 
         final = torch.cat(out)
@@ -43,6 +46,41 @@ class TransformerModel(torch.nn.Module):
         x = torch.nn.Linear()(x)
         torch.nn.Softmax()(x)
         return x
+
+
+class TransformerModel(torch.nn.Module):
+
+    def __init__(self, config, num_tokens, input_size, num_heads, hidden_size, num_layers, dropout=0.5):
+        super(TransformerModel, self).__init__()
+        self.pos_encoder = PositionalEncoding(input_size, dropout)
+        encoder_layers = TransformerEncoderLayer(input_size, num_heads, hidden_size, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, num_layers)
+        self.encoder = torch.nn.Embedding(num_tokens, input_size)
+        if config["decoder_type"].casefold() == "linear":  # PyTorch's example in docs uses linear decoder
+            self.transformer_decoder = torch.nn.Linear(input_size, num_tokens)
+        elif config["decoder_type"].casefold() == "decoder":
+            decoder_layers = TransformerDecoderLayer(input_size, num_heads, hidden_size, dropout)
+            self.transformer_decoder = TransformerDecoder(input_size, num_tokens)
+        self.ninp = input_size
+        self.init_weights()
+
+    def generate_square_subsequent_mask(self, sz):
+        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+        return mask
+
+    def init_weights(self):
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, src, src_mask):
+        src = self.encoder(src) * math.sqrt(self.ninp)
+        src = self.pos_encoder(src)
+        output = self.transformer_encoder(src, src_mask)
+        output = self.Transformerdecoder(output)
+        return output
 
 
 class PositionalEncoding(torch.nn.Module):
